f6d8ff03070d10515408289d124a26ffaeec9d29
billchenziang33 <billziang.chen@mail.utoronto.ca>
2025-10-12 09:54:47 +0800

    commit

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -14,15 +14,28 @@ import java.util.*;
  */
 public class CachingBreedFetcher implements BreedFetcher {
     // TODO Task 2: Complete this class
+    private final BreedFetcher fetcher;
+    private final Map<String, List<String>> cache = new HashMap<>();
     private int callsMade = 0;
-    public CachingBreedFetcher(BreedFetcher fetcher) {
 
+    public CachingBreedFetcher(BreedFetcher fetcher) {
+        this.fetcher = fetcher;
     }
 
     @Override
     public List<String> getSubBreeds(String breed) {
         // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+        if(cache.containsKey(breed)){
+            return cache.get(breed);
+        }
+        try{
+            List<String> subBreeds = fetcher.getSubBreeds(breed);
+            callsMade++;
+            cache.put(breed, subBreeds);
+            return subBreeds;
+        }catch(BreedNotFoundException e) {
+            throw e;
+        }
     }
 
     public int getCallsMade() {
--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -30,6 +30,30 @@ public class DogApiBreedFetcher implements BreedFetcher {
         //      to refer to the examples of using OkHttpClient from the last lab,
         //      as well as the code for parsing JSON responses.
         // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+        String url = "https://dog.ceo/api/breed/" + breed +"/list";
+
+        Request request = new Request.Builder().url(url).build();
+
+        try(Response response = client.newCall(request).execute()) {
+            String jsonResponse = response.body().toString();
+
+            JSONObject obj = new JSONObject(jsonResponse);
+
+            if (obj.getString("status").equals("error")) {
+                throw new BreedNotFoundException("Breed Not Found" + breed);
+            }
+
+            JSONArray subBreedsArray = obj.getJSONArray("message");
+            List<String> subBreeds = new ArrayList<>();
+            for (int i = 0; i < subBreedsArray.length(); i++) {
+                subBreeds.add(subBreedsArray.getString(i));
+            }
+
+            return subBreeds;
+
+        }catch (IOException e){
+            throw new BreedNotFoundException("Fail to fetch breed info" + e.getMessage());
+        }
+
     }
 }
